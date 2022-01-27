e89c3082f576f349cc7937a5ea53b17a9e40f081
kbly538 <kubilay.bilgili@gmail.com>
2022-01-27 22:15:53 +0300

    + Prepare migration for a custom view and a sproc

---
+++ AutoLot.Dal/EfStructures/Migrations/20220127185703_SQL.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoLot.Dal.EfStructures.Migrations
+{
+    public partial class SQL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            MigrationHelpers.CreateSprocs(migrationBuilder);
+            MigrationHelpers.CreateCustomerOrderView(migrationBuilder);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            MigrationHelpers.DropSproc(migrationBuilder);
+            MigrationHelpers.DropCustomerOrderView(migrationBuilder);
+        }
+    }
+}
