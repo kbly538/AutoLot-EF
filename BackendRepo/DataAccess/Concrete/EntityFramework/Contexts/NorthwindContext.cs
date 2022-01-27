6707db9b7f0c22a60c3f2c25969ab520301866fc
kbly538 <kubilay.bilgili@gmail.com>
2022-01-28 01:33:20 +0300

    + Initial Commit w/ Basics + Add Product Entity + EntityFramework Infrastructure

---
+++ BackendRepo/DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+	public class NorthwindContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Database=Northwind; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+			optionsBuilder.UseSqlServer(connectionString);
+		}
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
