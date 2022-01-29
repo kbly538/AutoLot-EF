fa7a5a60d0dbbc6a2ffbe128c439e9b4c2dd81d0
kbly538 <kubilay.bilgili@gmail.com>
2022-01-29 20:46:42 +0300

    + Add fixture class

---
+++ AutoLot.Dal.Tests/Base/EnsureAutoLotDatabasetTestFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoLot.Dal;
+using AutoLot.Dal.Initialization;
+
+namespace AutoLot.Dal.Tests.Base
+{
+	public sealed class EnsureAutoLotDatabasetTestFixture : IDisposable
+	{
+		public EnsureAutoLotDatabasetTestFixture()
+		{
+			var configuration = TestHelpers.GetConfiguration();
+			var context = TestHelpers.GetContext(configuration);
+			SampleData.ClearAndReseedDatabase(context);
+			context.Dispose();
+		}
+
+		public void Dispose(){}
+	}
+}
